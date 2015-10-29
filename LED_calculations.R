1b00c253dab1a772718303d000214fd66e775594
Julin Maloof <jnmaloof@ucdavis.edu>
2015-10-28 18:02:05 -0700

    fix nanometer problem

--- LED_calculations.R
+++ LED_calculations.R
@@ -1,6 +1,6 @@
 h <- 6.63e-34 # Planck's constant
 c <- 3e8 # speed of light m/s
-lambda <- 740 #wavelength
+lambda <- 740e-9 #wavelength in meters
 avogadro <- 6.022e23 # number of photons per mole
 
 Joules.per.photon <- h*c/lambda
@@ -33,3 +33,5 @@ area <- 4*pi*1^2 #(obviously that last term isn't needed since the distance is o
 uEinsteins.per.m2 <- uEinsteins/area
 
 uEinsteins.per.m2
+
+#since the LED is only eliminating half of a sphere this is probably 50% too low, but it gets us in the ballpark.
